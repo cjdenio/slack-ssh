48b6e56c9b89be3fd02adf7a3edccf5e1b51d875
Caleb Denio <cjdenio44@gmail.com>
2021-01-28 15:44:21 -0500

    :tada: Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const { App } = require("@slack/bolt");
+const { NodeSSH } = require("node-ssh");
+
+const CHANNEL = process.env.SLACK_CHANNEL;
+
+const ssh = new NodeSSH();
+
+ssh
+  .connect({
+    host: process.env.SSH_HOST,
+    username: process.env.SSH_USER,
+    privateKey: process.env.SSH_PRIVATE_KEY_PATH,
+  })
+  .then(() => {
+    console.log("connected");
+  });
+
+const app = new App({
+  appToken: process.env.SLACK_APP_TOKEN,
+  token: process.env.SLACK_BOT_TOKEN,
+  socketMode: true,
+});
+
+app.message(async ({ message, say }) => {
+  if (message.channel == CHANNEL) {
+    console.log(`executing: ${message.text}`);
+
+    const result = await ssh.execCommand(message.text);
+    let text = "_no text_";
+    if (result.stdout != "") {
+      text = result.stdout;
+    } else if (result.stderr != "") {
+      text = result.stderr;
+    }
+
+    await say(text);
+  }
+});
+
+(async () => {
+  await app.start();
+  console.log("⚡️ Bolt app started");
+})();
